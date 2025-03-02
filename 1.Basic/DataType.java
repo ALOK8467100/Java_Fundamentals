158388267feb4927789dfbe2ef42166fddd217de
alok <60954798+ALOK8467100@users.noreply.github.com>
2025-03-03 01:41:56 +0530

    Add files via upload

---
+++ 1.Basic/DataType.java
@@ -0,0 +1,49 @@
+public class DataType {
+  public static void main(String a[])
+  {
+    System.out.print("Hello Data Type");
+
+    // Now Data Types
+
+    // int, float, double, char, boolean, String
+
+    // int
+    int num = 10;
+    System.out.println(num);
+
+    // float
+    float num1 = 10.5f;
+    System.out.println(num1);
+
+    // double
+    double num2 = 10.5;
+    System.out.println(num2);
+
+    // char
+    char ch = 'a';
+    System.out.println(ch);
+
+    // boolean
+    boolean flag = true;
+    System.out.println(flag);
+
+    // String
+    String str = "Hello";
+    System.out.println(str);
+
+    // byte
+    byte b = 10;
+    System.out.println(b);
+
+    // long
+    long l = 1000000000000000000L;
+    System.out.println(l);
+
+    // short
+    short s = 10;
+    System.out.println(s);
+
+    
+
+  }
+}
